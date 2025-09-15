725bc4838346d6b985257e642db7322fe5e68440
vlsribeiro <vlsribeiro@apoiocdhu.sp.gov.br>
2025-09-15 14:33:56 -0300

    Add files via upload

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -757,98 +757,20 @@ function fnc_project_color(values, context) {
 
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule0_eval_expression(context) {
-    // "Mudan�a realizada?" = 'Sim'
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule1_eval_expression(context) {
+    // setor  IN ('05', '06') AND geocodigo IS NOT NULL AND tip_nume IS NULL OR setor = '03' AND geocodigo IS NOT NULL AND tip_nume IS NULL AND quadra = 'A' AND (domicilio NOT IN ('0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108') OR domicilio = '0085' AND digito = 'A')
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return (feature.properties['Mudan�a realizada?']  == 'Sim');
+        return ((("'05', '06'".indexOf(feature.properties['setor'] ) > -1  && (feature.properties['geocodigo']  !== null)) && (feature.properties['tip_nume']  === null)) || (((((feature.properties['setor']  == '03') && (feature.properties['geocodigo']  !== null)) && (feature.properties['tip_nume']  === null)) && (feature.properties['quadra']  == 'A')) && (!"'0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108'".indexOf(feature.properties['domicilio'] ) > -1  || ((feature.properties['domicilio']  == '0085') && (feature.properties['digito']  == 'A')))));
     } else {
-        return (feature['Mudan�a realizada?']  == 'Sim');
+        return ((("'05', '06'".indexOf(feature['setor'] ) > -1  && (feature['geocodigo']  !== null)) && (feature['tip_nume']  === null)) || (((((feature['setor']  == '03') && (feature['geocodigo']  !== null)) && (feature['tip_nume']  === null)) && (feature['quadra']  == 'A')) && (!"'0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108'".indexOf(feature['domicilio'] ) > -1  || ((feature['domicilio']  == '0085') && (feature['digito']  == 'A')))));
     }
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule1_eval_expression(context) {
-    // "Classifica��o 2" = 'Contrato assinado - A13' OR "Classifica��o 2" = 'Contrato Assinado - A13' AND "Mudan�a realizada?" <> 'Sim'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['Classifica��o 2']  == 'Contrato assinado - A13') || ((feature.properties['Classifica��o 2']  == 'Contrato Assinado - A13') && (feature.properties['Mudan�a realizada?']  != 'Sim')));
-    } else {
-        return ((feature['Classifica��o 2']  == 'Contrato assinado - A13') || ((feature['Classifica��o 2']  == 'Contrato Assinado - A13') && (feature['Mudan�a realizada?']  != 'Sim')));
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule2_eval_expression(context) {
-    // "Classifica��o 2" = 'Sorteado - A13'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Sorteado - A13');
-    } else {
-        return (feature['Classifica��o 2']  == 'Sorteado - A13');
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule3_eval_expression(context) {
-    // "Classifica��o 2" = 'Habilitado - A13' OR "Classifica��o 2" = 'Habilitado (cadin) - A13'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['Classifica��o 2']  == 'Habilitado - A13') || (feature.properties['Classifica��o 2']  == 'Habilitado (cadin) - A13'));
-    } else {
-        return ((feature['Classifica��o 2']  == 'Habilitado - A13') || (feature['Classifica��o 2']  == 'Habilitado (cadin) - A13'));
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule4_eval_expression(context) {
-    // "Classifica��o 2" = 'Aferi��o UH' AND Especificar = 'A13'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['Classifica��o 2']  == 'Aferi��o UH') && (feature.properties['Especificar']  == 'A13'));
-    } else {
-        return ((feature['Classifica��o 2']  == 'Aferi��o UH') && (feature['Especificar']  == 'A13'));
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule5_eval_expression(context) {
-    // "Classifica��o 2" = 'Habilitado - A32'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Habilitado - A32');
-    } else {
-        return (feature['Classifica��o 2']  == 'Habilitado - A32');
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule6_eval_expression(context) {
-    // "Classifica��o 2" = 'Aferi��o UH' AND Especificar = 'A32'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['Classifica��o 2']  == 'Aferi��o UH') && (feature.properties['Especificar']  == 'A32'));
-    } else {
-        return ((feature['Classifica��o 2']  == 'Aferi��o UH') && (feature['Especificar']  == 'A32'));
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule7_eval_expression(context) {
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule2_eval_expression(context) {
     // "Classifica��o 2" = 'Aferi��o AM'
 
     var feature = context.feature;
@@ -861,59 +783,33 @@ function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule8_eval_expression(context) {
-    // "Classifica��o 2" = 'Iniciar Habilita��o'
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule3_eval_expression(context) {
+    // "Classifica��o 2" = 'Aferi��o UH' AND Especificar = 'A13'
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Iniciar Habilita��o');
+        return ((feature.properties['Classifica��o 2']  == 'Aferi��o UH') && (feature.properties['Especificar']  == 'A13'));
     } else {
-        return (feature['Classifica��o 2']  == 'Iniciar Habilita��o');
+        return ((feature['Classifica��o 2']  == 'Aferi��o UH') && (feature['Especificar']  == 'A13'));
     }
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule9_eval_expression(context) {
-    // "Classifica��o 2" = 'Indeciso'
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule4_eval_expression(context) {
+    // "Classifica��o 2" = 'Aferi��o UH' AND Especificar = 'A32'
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Indeciso');
+        return ((feature.properties['Classifica��o 2']  == 'Aferi��o UH') && (feature.properties['Especificar']  == 'A32'));
     } else {
-        return (feature['Classifica��o 2']  == 'Indeciso');
+        return ((feature['Classifica��o 2']  == 'Aferi��o UH') && (feature['Especificar']  == 'A32'));
     }
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule10_eval_expression(context) {
-    // "Classifica��o 2" = 'Pendente'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Pendente');
-    } else {
-        return (feature['Classifica��o 2']  == 'Pendente');
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule11_eval_expression(context) {
-    // "Classifica��o 2" = 'Estudo de caso'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'Estudo de caso');
-    } else {
-        return (feature['Classifica��o 2']  == 'Estudo de caso');
-    }
-}
-
-
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule12_eval_expression(context) {
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule5_eval_expression(context) {
     // "Classifica��o 2" = 'Ausente'
 
     var feature = context.feature;
@@ -926,33 +822,72 @@ function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule13_eval_expression(context) {
-    // "Classifica��o 2" = 'N�o mobilizado'
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule6_eval_expression(context) {
+    // "Classifica��o 2" = 'Estudo de caso'
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return (feature.properties['Classifica��o 2']  == 'N�o mobilizado');
+        return (feature.properties['Classifica��o 2']  == 'Estudo de caso');
     } else {
-        return (feature['Classifica��o 2']  == 'N�o mobilizado');
+        return (feature['Classifica��o 2']  == 'Estudo de caso');
     }
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule14_eval_expression(context) {
-    // "Classifica��o 2" = 'Sem ades�o' OR "Classifica��o 2" = 'Sem Ades�o'
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule7_eval_expression(context) {
+    // "Classifica��o 2" = 'Habilitado - A13' OR "Classifica��o 2" = 'Habilitado (cadin) - A13'
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return ((feature.properties['Classifica��o 2']  == 'Sem ades�o') || (feature.properties['Classifica��o 2']  == 'Sem Ades�o'));
+        return ((feature.properties['Classifica��o 2']  == 'Habilitado - A13') || (feature.properties['Classifica��o 2']  == 'Habilitado (cadin) - A13'));
     } else {
-        return ((feature['Classifica��o 2']  == 'Sem ades�o') || (feature['Classifica��o 2']  == 'Sem Ades�o'));
+        return ((feature['Classifica��o 2']  == 'Habilitado - A13') || (feature['Classifica��o 2']  == 'Habilitado (cadin) - A13'));
     }
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule15_eval_expression(context) {
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule8_eval_expression(context) {
+    // "Classifica��o 2" = 'Habilitado - A32'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Habilitado - A32');
+    } else {
+        return (feature['Classifica��o 2']  == 'Habilitado - A32');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule9_eval_expression(context) {
+    // "Classifica��o 2" = 'Sorteado - A13'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Sorteado - A13');
+    } else {
+        return (feature['Classifica��o 2']  == 'Sorteado - A13');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule10_eval_expression(context) {
+    // "Classifica��o 2" = 'Indeciso'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Indeciso');
+    } else {
+        return (feature['Classifica��o 2']  == 'Indeciso');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule11_eval_expression(context) {
     // "Classifica��o 2" = 'Indeferido+Comercio' OR "Classifica��o 2" = 'Indeferido+Com�rcio' OR "Classifica��o 2" = 'Indeferido+Constru��o' OR "Classifica��o 2" = 'Indeferido+Vaga'
 
     var feature = context.feature;
@@ -965,7 +900,85 @@ function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule16_eval_expression(context) {
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule12_eval_expression(context) {
+    // "Classifica��o 2" = 'Contrato assinado - A13' OR "Classifica��o 2" = 'Contrato Assinado - A13'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Classifica��o 2']  == 'Contrato assinado - A13') || (feature.properties['Classifica��o 2']  == 'Contrato Assinado - A13'));
+    } else {
+        return ((feature['Classifica��o 2']  == 'Contrato assinado - A13') || (feature['Classifica��o 2']  == 'Contrato Assinado - A13'));
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule13_eval_expression(context) {
+    // "Classifica��o 2" = 'Sorteado - A32'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Sorteado - A32');
+    } else {
+        return (feature['Classifica��o 2']  == 'Sorteado - A32');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule14_eval_expression(context) {
+    // "Classifica��o 2" = 'Visita Domiciliar'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Visita Domiciliar');
+    } else {
+        return (feature['Classifica��o 2']  == 'Visita Domiciliar');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule15_eval_expression(context) {
+    // "Classifica��o 2" = 'Iniciar Habilita��o'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Iniciar Habilita��o');
+    } else {
+        return (feature['Classifica��o 2']  == 'Iniciar Habilita��o');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule16_eval_expression(context) {
+    // "Classifica��o 2" = 'Pendente'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'Pendente');
+    } else {
+        return (feature['Classifica��o 2']  == 'Pendente');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule17_eval_expression(context) {
+    // "Classifica��o 2" = 'Sem ades�o' OR "Classifica��o 2" = 'Sem Ades�o'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['Classifica��o 2']  == 'Sem ades�o') || (feature.properties['Classifica��o 2']  == 'Sem Ades�o'));
+    } else {
+        return ((feature['Classifica��o 2']  == 'Sem ades�o') || (feature['Classifica��o 2']  == 'Sem Ades�o'));
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule18_eval_expression(context) {
     // "Classifica��o 2" = 'Troca Autorizada'
 
     var feature = context.feature;
@@ -978,14 +991,40 @@ function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule
 }
 
 
-function exp_AcompanhamentodehabilitacaoemudanasrealizadasAPAIguatemiFase1_3rule17_eval_expression(context) {
-    // setor  IN ('05', '06') AND geocodigo IS NOT NULL AND tip_nume IS NULL OR setor = '03' AND geocodigo IS NOT NULL AND tip_nume IS NULL AND quadra = 'A' AND (domicilio NOT IN ('0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108') OR domicilio = '0085' AND digito = 'A')
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule19_eval_expression(context) {
+    // "Classifica��o 2" = 'Mobilizado'
 
     var feature = context.feature;
     
     if (feature.properties) {
-        return ((("'05', '06'".indexOf(feature.properties['setor'] ) > -1  && (feature.properties['geocodigo']  !== null)) && (feature.properties['tip_nume']  === null)) || (((((feature.properties['setor']  == '03') && (feature.properties['geocodigo']  !== null)) && (feature.properties['tip_nume']  === null)) && (feature.properties['quadra']  == 'A')) && (!"'0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108'".indexOf(feature.properties['domicilio'] ) > -1  || ((feature.properties['domicilio']  == '0085') && (feature.properties['digito']  == 'A')))));
+        return (feature.properties['Classifica��o 2']  == 'Mobilizado');
     } else {
-        return ((("'05', '06'".indexOf(feature['setor'] ) > -1  && (feature['geocodigo']  !== null)) && (feature['tip_nume']  === null)) || (((((feature['setor']  == '03') && (feature['geocodigo']  !== null)) && (feature['tip_nume']  === null)) && (feature['quadra']  == 'A')) && (!"'0077', '0080', '0081', '0084', '0085', '0089', '0106', '0107', '0108'".indexOf(feature['domicilio'] ) > -1  || ((feature['domicilio']  == '0085') && (feature['digito']  == 'A')))));
+        return (feature['Classifica��o 2']  == 'Mobilizado');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule20_eval_expression(context) {
+    // "Classifica��o 2" = 'N�o mobilizado'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Classifica��o 2']  == 'N�o mobilizado');
+    } else {
+        return (feature['Classifica��o 2']  == 'N�o mobilizado');
+    }
+}
+
+
+function exp_S042_SSARUDAH_P_APA_Iguatemi_Edificacoes_01_Acompanhamento_de_habilitacao_Primeira_Fase_Classes_Detalhadascopiar_3rule21_eval_expression(context) {
+    // "Mudan�a realizada?" = 'Sim'
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return (feature.properties['Mudan�a realizada?']  == 'Sim');
+    } else {
+        return (feature['Mudan�a realizada?']  == 'Sim');
     }
 }
